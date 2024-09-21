9004e996f1b6e9631042a17ea97801489454e501
Sainath-Kumar <krish199121@gmail.com>
2024-09-21 23:18:55 +0530

    Add files via upload

---
+++ week1/day2/EdgeBrowser.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		Browser M=new Browser();
+		M.launchBrowser("Edge");
+		M.loadUrl();
+		M.browserName();
+	}
+
+}
---
+++ week1/day2/FindOddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args)
+	{
+		int maxRange = 10;
+				for (int i = 0; i<= maxRange; i++)
+				{
+				if (i%2 != 0) 
+				{
+					System.out.println("Odd Number : "+i);
+				}
+				}
+	}
+
+}
